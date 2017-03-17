ef2ff4550b33adafb0747ecad1db93a42dd6d08c
David Vuong <david.vuong256@gmail.com>
2017-03-18 01:15:19 +1100

    refactor(http_api/Boot): add type annotations

--- services/http_api/src/main/scala/me/davidvuong/http_api/Boot.scala
+++ services/http_api/src/main/scala/me/davidvuong/http_api/Boot.scala
@@ -1,15 +1,16 @@
 package me.davidvuong.http_api
 
-import org.http4s.server.ServerApp
+import org.http4s.server.{Server, ServerApp}
 import org.http4s.server.blaze.BlazeBuilder
-
 import me.davidvuong.http_api.config.Config
+
+import scalaz.concurrent._
 
 object Boot extends ServerApp {
 
   val config = Config.loadUnsafe
 
-  def server(args: List[String]) = {
+  def server(args: List[String]): Task[Server] = {
     BlazeBuilder
       .bindHttp(config.http.port, config.http.host)
       .start
