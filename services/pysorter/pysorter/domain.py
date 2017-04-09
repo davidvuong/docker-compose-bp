dc3add1592733c4e75b596981c4ba315d8745274
David Vuong <david.vuong256@gmail.com>
2017-04-10 00:50:50 +1000

    feat(pysorter): add initial work for pysorter
    1. Pysorter is just another SQS worker, reading from a queue and then
    basically sorting the incoming message.
    2. This Python project makes use of Python v3.6.1 which means we have
    really nice type annotations. I'm using mypy to statically analyse the
    code. There's a problem ATM where it's analysing vendor code (boto3)
    and I need to disable that
    3. The code right now doesn't do anything and most likely doesn't work.
    I still need to figure out a way for it to play nice with PyCharm.

---
+++ services/pysorter/pysorter/domain.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from typing import Tuple, Dict
+
+
+class AwsAuthentication:
+    def __init__(self, access_key: str, secret_key: str) -> None:
+        self.access_key: str = access_key
+        self.secret_key: str = secret_key
+
+
+class SqsConfig:
+    def __init__(self, endpoint_url: str, port: int, region: str, use_ssl: bool = False) -> None:
+        self.endpoint_url = endpoint_url
+        self.port = port
+        self.region = region
+        self.use_ssl = use_ssl
+
+
+class IngressMessage:
+    def to_dict(self) -> Dict:
+        pass
+
+
+class EgressMessage:
+    def to_dict(self) -> Dict:
+        pass
---
+++ services/pysorter/pysorter/sqs_adaptor.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import json
+
+from typing import Callable, Union, Dict
+
+import boto3  # type: ignore
+
+from .domain import AwsAuthentication, SqsConfig
+from .domain import EgressMessage
+
+SendMessageHandler = Callable[[Union[Exception, Dict]], None]
+
+
+def connect(config: SqsConfig, auth: AwsAuthentication):
+    return boto3.resource('sqs',
+        endpoint_url=config.endpoint_url,
+        region_name=config.region,
+        aws_secret_access_key=auth.secret_key,
+        aws_access_key_id=auth.access_key,
+        use_ssl=config.use_ssl
+    )
+
+
+def send_message(client, queue_url: str, message: EgressMessage) -> None:
+    return client.send_message(
+        QueueUrl=queue_url,
+        MessageBody=json.dumps(message.to_dict())
+    )
+
+
+def receive_message(client, queue_url: str, handler: SendMessageHandler) -> None:
+    while True:
+        try:
+            response = client.receive_message(
+                QueueUrl=queue_url,
+                WaitTimeSeconds=10,
+                VisibilityTimeout=60,
+                MaxNumberOfMessages=10
+            )
+        except Exception as exception:
+            response = exception
+        handler(response)
